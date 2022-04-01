d1b056c1e23fbb00d364502e7a5077285222cc4a
pomidoorrka <100994626+pomidoorrka@users.noreply.github.com>
2022-04-01 11:40:25 +0500

    Update tetris.cpp

--- tetris.cpp
+++ tetris.cpp
@@ -13,9 +13,66 @@ using namespace sf;
     2,3,5,7,
     3,5,6,7,
     2,3,4,5,};
+    struct koordinati_kubika{
+    int x,y;
+    }tek[4], pred[4];
+    bool test(){
+        for (int i = 0; i < 4; i+=){
+            if(tek[i].x < 0 | tek[i].x > width | tek [i].y > height)
+                return 0; // 0 - всё работает, 1 - ты дебил
+            else
+                return 1;
+        }
+    }
     int main(){
         srand(time(0));
+        RenderWindow window1(VideoMode(wight*size,height*size) ,"JOCKIETETRIS");
         Texture Kubik;
         Kibik.loadFromFile("/*сюды фоточку загружать(обсалютной ссылкой)*/");
         Sprite sprite_figuri(Kubik);
- }
+        bool rotate = false;
+        int stepX = 0;
+        float delay = 0.5;
+        while (window1,isOpen()){
+            Event doing;
+                while(window1.pollEvent(doing)){
+                    if (doing.type==Event::KeyPressed)
+                        if (doing.key.code==Keyboard::Up)
+                            rotate=true;
+                        else if(doing.type==Event::KeyPressed)
+                            if (doing.key.code==Keyboard::Right)
+                                stepX = 1;
+                            else (doing.type==Event::KeyPressed)
+                        if (doing.key.code==Keyboard::Left)
+                                stepX = -1;
+                        if (doing.type == Event::Closed)
+                            window1.close();
+     }
+                        if (Keyboard::isKeyPressed (Keyboard::Down) )
+                            delay = delay/2;
+    
+                        for (int i = 0; i < 4; i+=){
+                        pred[i]=tek[i];
+                        tek[i].x += stepX;
+                        }
+                        if (test () == true){
+                            int i = 0; i < 4; i+=)
+                                tek[i] = pred[i];
+                        }  
+            //задали первую фигуру
+            if(tek[0].x==0){
+            int n = rand() % 7;
+                for (int i = 0; i < 4; i+=1){
+                    tek[i].x = kubiki[n][i] % 2;
+                    tek[i].y = kubiki[n][i] / 2;
+                }
+            }
+            window1.clear(Color::White);
+            for(int i = 0; i < 4; i+=1){
+            sprite_figuri.setTextureRect(intrect(0, size, size, size));
+            sprite_figuri.setPosition(tek[i].x*size, tek[i].y*size);
+            window1.draw(sprite_figuri);
+            }
+            window1.display();
+                }
+    }
