568e76a9e750d4bb3c039af33e57077b6e6ad6ac
pomidoorrka <100994626+pomidoorrka@users.noreply.github.com>
2022-04-09 13:37:36 +0500

    Create Sortirovka_s_prikolami_beta.cpp

---
+++ Sortirovka_s_prikolami_beta.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <ctime>
+#include <chrono>
+
+using namespace std;
+void sort(int* formalArr, int left, int right) {
+  int mid
+
+}
+int main{
+  srand
+  int n, i, left, right;
+  setlocale(LC_ALL, "Rus")
+    cout
+    cin
+    array
+    left = 0;
+    right = n - 1;
+  for
+    a[i] = [19;42]
+    cout
+    auto start = chrono::high_resolution_clock::now();
+    sort(a, left, right);
+    auto stop = chrono::high_resolution_clock::now();
+    chrono::duration <float> timer = (stop - start);
+    for
+    cout
+    cout << "time: " << timer.count() << "seconds";
+    system ('pause')
+}
