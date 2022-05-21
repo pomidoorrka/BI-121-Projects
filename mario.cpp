f42d508f91bf42c56bd17ffe7362d66389821d79
pomidoorrka <100994626+pomidoorrka@users.noreply.github.com>
2022-05-21 13:39:11 +0500

    Update mario.cpp

--- mario.cpp
+++ mario.cpp
@@ -3,7 +3,54 @@
 
 using namespase sf;
 
+class Fazer{
+  public:
+    float x, y, stepx, stepy, speed, w, h;
+    int cuda;
+    Image image;
+    Texture texture;
+    Sprite sprite;
+    string name;
+    Fazer (string N, float W, float H, float X, float Y){
+      stepX = 0;
+      stepY = 0;
+      speed = 0;
+      cuda = 0;
+      name = n;
+      w = W;
+      h = H;
+      x = X;
+      y = Y;
+      image.loadfromfile ("Указываем папку с картинками/" + name);
+      image.createMaskFromColor(Color(255, 255, 255));
+      texture.loadFromImage(image);
+      sprite.setTexture(texture);
+      sprite.setTextureRect(IntRect(0, 0, w, h));
+    }
+  void moveT(float time){
+    switch (move){
+      case 0:
+        stepX = speed;
+        stepY = 0;
+        break;
+      case 1:
+        stepX = -speed;
+        stepY = 0;
+        break;
+      case 2:
+        stepX = 0;
+        stepY = -speed;
+        break;
+    }
+    x += stepX * time;
+    y += stepY * time;
+    speed = 0;
+    sprite.setPosition(x, y+280);
+  }
+};
+
 int main{
+  Fazer m("Текстурка марио", 0, 0, 64, 85);
   int height = 17, width = 240;
   String map[height] = {
 "ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss";,
@@ -27,14 +74,14 @@ int main{
   icon.loadFromFile("путь к фавикону");
   window.setIcon("разрешение, фавикона", icon.getPixelsPtr());
   int size = 32;
-  Image hero_image;
+  /*Image hero_image;
   hero_image.loafFromFile("хироэмаге ");
   hero_image.createMaskFromColor(Color(255, 255, 255));
   Texture HERO_TEXTURE;
   HERO_TEXTURE.loadFromFile(hero_image");                       //тайл спрайт на спрайте на спрайте на спрайте
   Sprite HERO_SPRITE(HERO_TEXTURE);
   HERO_SPRITE.serTextureRect(IntRect(0, 0, 64, 85));
-  HERO_SPRITE.setPosition(0, 360);
+  HERO_SPRITE.setPosition(0, 360);*/
   
   Texture map_texture;
   map_texture.loadFromFile("фон");
@@ -53,7 +100,7 @@ int main{
       if (event.type == Event::Closed)
         window1.close();
     }
-    if (Keyboard::isKeyPressed(Keyboard::Right)){
+    /*if (Keyboard::isKeyPressed(Keyboard::Right)){
       HERO_SPRITE.setScale(1, 1);
       nowFrame += 0.01 * time;               // nowFrame = nowFrame + (0,001 * time)
       if(nowFrame > 4)
@@ -72,7 +119,33 @@ int main{
     if (Keyboard::isKeyPressed(Keyboard::Up)){
       HERO_SPRITE.move(0, time * -0.1);
       HERO_SPRITE.setTextureRext(intrect(418, 0, 85, 85));
+    }*/
+    if(Keyboard::isKeyPressed(Keyboard::Right)){
+    m.move = 0;
+    m.speed = 0,2;
+    nowFrame += 0.01 * time;               // nowFrame = nowFrame + (0,001 * time)
+      if(nowFrame > 4)
+        nowFrame -= 4;
+      m.sptire.setTextureRect(IntRect(80 * int (nofFrame), 0, 80, 85));
     }
+    if(Keyboard::isKeyPressed(Keyboard::Left)){
+    m.sprite.setScale (-1,1);
+    m.move = 1;
+    m.speed = 0,2;
+    nowFrame += 0.01 * time;               // nowFrame = nowFrame + (0,001 * time)
+      if(nowFrame > 4)
+        nowFrame -= 4;
+      m.sptire.setTextureRect(IntRect(80 * int (nofFrame), 0, 80, 85));
+    }
+    if(Keyboard::isKeyPressed(Keyboard::Up)){
+    m.move = 2;
+    m.speed = 0,2;
+    nowFrame += 0.01 * time;               // nowFrame = nowFrame + (0,001 * time)
+      if(nowFrame > 4)
+        nowFrame -= 4;
+      m.sptire.setTextureRect(IntRect(418, 0, 80, 85));
+    }
+    m.moveT(time);
     window1.clear();
     for (int i = 0; i < height ;i++)
       for(int j = 0; j < width; j++){
@@ -89,7 +162,7 @@ int main{
         map_sprite.setPosition(i * size, j * size);                       ////////////
         window1.draw(map_sprite);
       }
-    window1.draw(HERO_SPRITE);
+    window1.draw(m.sprite);
     window1.display();
   }
 }
